8f4d0456911237f57433981b459af70c0377c811
Cole Uhlig <cole@uhlig.io>
2022-09-10 08:47:08 -0700

    single controller

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,10 @@
 #define INTAKE_PORT 7
 #define EXPANSION_PORT 'A'
 #define INDEXER_PORT 'B'
+#define BLOCKER_PORT 'C'
 #define STEP_SIZE 20
+using namespace pros;
+using namespace std;
 
 typedef enum {
   wait,
@@ -24,12 +27,15 @@ typedef enum {
 
 void opcontrol() {
   bool flywheel_status = false;
-  bool intake_status = false;
-  int flywheel_speed = 100;
+  int intake_status = 0;
+  int flywheel_speed = 600;
   pneumatic state = wait;
   bool flywheel_up_pressed = false;
   bool flywheel_down_pressed = false;
+  bool intake_up_pressed = false;
+  bool intake_down_pressed = false; 
   bool indexer_status = false;
+  int flywheel_spin = 0;
   auto time_start = std::chrono::system_clock::now();
   pros::Motor left_back (LEFT_BACK_PORT,true);
   pros::Motor left_front (LEFT_FRONT_PORT); 
@@ -40,8 +46,10 @@ void opcontrol() {
   pros::Motor intake (INTAKE_PORT, MOTOR_GEARSET_06);
   pros::ADIDigitalOut expansion (EXPANSION_PORT);
   pros::ADIDigitalOut indexer (INDEXER_PORT);
+  pros::ADIDigitalOut blocker (BLOCKER_PORT);
   pros::Controller master (CONTROLLER_MASTER);
 
+  expansion.set_value(true);
 
   while (true) {
 	  std::int8_t left_joystick = master.get_analog(ANALOG_LEFT_Y);
@@ -67,42 +75,75 @@ void opcontrol() {
     } else {
       flywheel_up_pressed = false;
     }
+    flywheel1.move_velocity(flywheel_spin);
+    flywheel2.move_velocity(flywheel_spin);
 
     if (master.get_digital(DIGITAL_UP)) {
-      flywheel1.move_velocity(flywheel_speed);
-      flywheel2.move_velocity(flywheel_speed);
+      flywheel_spin=flywheel_speed;
     }
+
     if (master.get_digital(DIGITAL_DOWN)) {
-      flywheel1.move_velocity(0);
-      flywheel2.move_velocity(0);
+      flywheel_spin=0; 
     }
-    if (master.get_digital(DIGITAL_X)) {
+
+    //intake
+    if (master.get_digital(DIGITAL_R2)) {
+      if (!intake_down_pressed){
+        if (intake_status == -1){
+          intake_status = 0;
+        }
+        else if (intake_status == 0){
+          intake_status = -1;
+        }
+         intake_down_pressed = true;
+      }
+     
+    } else {
+      intake_down_pressed = false;
+    }
+    if (master.get_digital(DIGITAL_R1)) {
+      if (!intake_up_pressed){
+        if (intake_status == 1){
+          intake_status = 0;
+        }
+        else if (intake_status == 0){
+          intake_status = 1;
+        }
+        intake_up_pressed = true;
+      }
+    } else {
+      intake_up_pressed = false;
+    }
+
+
+    if (intake_status==1) {
       intake.move_velocity(10000);
     }
-    if (master.get_digital(DIGITAL_B)) {
+    if (intake_status==0) {
       intake.move_velocity(0);
     }
-    
-    //statemachine
-    auto current = std::chrono::system_clock::now();
-
-    if(state == wait && master.get_digital(DIGITAL_A)) {
-      state = expanding;
-      time_start = current;
-      indexer.set_value(true);
+    if (intake_status==-1) {
+      intake.move_velocity(-100000);
     }
     
-    std::chrono::duration<double> elapsed_seconds = current - time_start;
-
-    if(state == expanding && elapsed_seconds.count() > 0.5){
-      state = retracting;
-      time_start = current;
+    //indexer
+    if(master.get_digital(DIGITAL_A)) {
+      blocker.set_value(false);
+      pros::delay(20);
       indexer.set_value(true);
     }
-
-    if(state == retracting && elapsed_seconds.count() > 0.5){
-      state = wait;
+    if(!master.get_digital(DIGITAL_A)) {
+      blocker.set_value(true);
+      pros::delay(20);
+      indexer.set_value(false);
     }
+    //expansion
+
+    if(master.get_digital(DIGITAL_B)) {
+      expansion.set_value(false);
+    }
+    
+    //print
     master.print(0,0,"%5d %d",flywheel_speed, state);
 
   
